1b537e8b2c7d399a72eb7efb9257e5698424f9aa
Zharkyngali <zh.sabanbay@gamil.com>
2021-02-07 22:15:46 +0600

    Complete the News App Project

---
+++ NewsApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  NewsApp
+//
+//  Created by Zharkyngali Savanbay on 06.02.2021.
+//  Copyright © 2021 Zharkyngali Savanbay. All rights reserved.
+//
+
+
+struct K {
+    static let cellIdentifier = "ReusableCell"
+}
